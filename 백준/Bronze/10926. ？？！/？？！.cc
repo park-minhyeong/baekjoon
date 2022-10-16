0cdc2567428d219f244c5896280691e321b6c548
IT_duncan <105710796+duncan1409@users.noreply.github.com>
2022-10-16 02:53:35 -0700

    [Bronze V] Title: ??!, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+  //OOP code
+  string saying;
+  cin>> saying;
+  saying.append("??!");
+  cout<<saying;
+
+  return 0;
+}
