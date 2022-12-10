f2523f888d7172b4ebef1c68595c0bec43ace2be
IT_duncan <105710796+duncan1409@users.noreply.github.com>
2022-12-10 09:14:46 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+	struct tm *t;
+	time_t timer;
+	timer = time(NULL);
+	t = localtime(&timer);
+
+	cout << t->tm_year + 1900 << "-";
+	cout.width(2);
+	cout.fill('0');
+	cout << t->tm_mon + 1 << "-" << t->tm_mday;
+
+	return 0;
+}
