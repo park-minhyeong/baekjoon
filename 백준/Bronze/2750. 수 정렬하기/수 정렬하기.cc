5464df231013a8bd19fb22bf58c2d79542f452c8
IT_duncan <105710796+duncan1409@users.noreply.github.com>
2023-02-02 19:17:12 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.cc
@@ -1,24 +1,37 @@
 #include <iostream>
+#include <string>
+#include <cstring>
 #include <vector>
 #include <algorithm>
+#define endl "\n"
 using namespace std;
 
+void Solution(int N)
+{
+	vector<int> v;
+	for (int i = 0; i < N; i++)
+	{
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+	sort(v.begin(), v.end());
+
+	for (auto iter = v.begin(); iter != v.end(); iter++)
+	{
+		cout << *iter << endl;
+	}
+}
 int main()
 {
-  ios::sync_with_stdio(false);
-  cin.tie(NULL);
-  cout.tie(NULL);
-  int N;
-  cin >> N;
-  vector<int> v;
-  while (N--)
-  {
-    int num;
-    cin>>num;
-    v.push_back(num);
-  }
-  sort(v.begin(), v.end());
-  for (auto loop: v)
-    cout<<loop<<endl;
-  return 0;
+	//freopen("input.txt", "r", stdin);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+	Solution(N);
+
+	return 0;
 }
