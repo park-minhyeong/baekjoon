4654ed7d8a091579dc0596e79edf5abb79f218de
IT_duncan <105710796+duncan1409@users.noreply.github.com>
2023-02-12 00:04:58 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <cmath>
+#define endl "\n"
+using namespace std;
+
+void Solution()
+{
+	cout << "     /~\\" << endl;
+	cout << "    ( oo|" << endl;
+	cout << "    _\\=/_" << endl;
+	cout << "   /  _  \\" << endl;
+	cout << "  //|/.\\|\\\\" << endl;
+	cout << " ||  \\ /  ||" << endl;
+	cout << "============" << endl;
+	cout << "|          |" << endl;
+	cout << "|          |" << endl;
+	cout << "|          |" << endl;
+}
+int main()
+{
+	//freopen("input.txt", "r", stdin);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// input 입력
+
+	Solution();
+	return 0;
+}
