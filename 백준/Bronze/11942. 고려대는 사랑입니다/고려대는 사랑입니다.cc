049dc6538ea326b29cb21a15267f70be787ed00a
IT_duncan <105710796+duncan1409@users.noreply.github.com>
2023-02-11 23:59:00 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <cmath>
+#define endl "\n"
+using namespace std;
+
+void Solution()
+{
+	cout << "고려대학교";
+}
+int main()
+{
+	//freopen("input.txt", "r", stdin);
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	// input 입력
+
+	Solution();
+	return 0;
+}
