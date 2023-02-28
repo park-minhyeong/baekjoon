8a5b11e0abdbe69e2efd1ad630c7ccc294cf7447
IT_duncan <105710796+duncan1409@users.noreply.github.com>
2023-02-28 23:49:31 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+#include <cmath>
+#define endl "\n"
+using namespace std;
+
+void Solution()
+{
+    cout<<"SHIP NAME      CLASS          DEPLOYMENT IN SERVICE"<<endl;
+    cout<<"N2 Bomber      Heavy Fighter  Limited    21        "<<endl;
+    cout<<"J-Type 327     Light Combat   Unlimited  1         "<<endl;
+    cout<<"NX Cruiser     Medium Fighter Limited    18        "<<endl;
+    cout<<"N1 Starfighter Medium Fighter Unlimited  25        "<<endl;
+    cout<<"Royal Cruiser  Light Combat   Limited    4         "<<endl;
+}
+int main()
+{
+  //  freopen("input.txt", "r", stdin);
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    // input 입력
+    
+    Solution();
+    return 0;
+}
